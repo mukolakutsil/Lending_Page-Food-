d35f2c3cf60baed5a75672e0ebca6785f325e0f5
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-01-26 22:27:24 +0200

    tabs

---
+++ js/script.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    let tabs = document.querySelectorAll('.tabheader__item'),
+        tabsParent = document.querySelector('.tabheader'),
+        tabsContent = document.querySelectorAll('.tabcontent');
+
+
+    let hideTabsContent = () => {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    };
+
+    let showTabContent = (i = 0) => {
+        tabs[i].classList.add('tabheader__item_active');
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+
+    };
+
+    hideTabsContent();
+    showTabContent();
+
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabsContent();
+                    showTabContent(i);
+                }
+            });
+        }
+
+    });
+});

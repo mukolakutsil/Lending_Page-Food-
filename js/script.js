176d851b24392b2af0048dfee171fbaad2341fbc
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-01-27 19:10:19 +0200

    timer

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Tabs
 
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsParent = document.querySelector('.tabheader'),
@@ -40,4 +41,66 @@ document.addEventListener('DOMContentLoaded', () => {
         }
 
     });
+
+    // Timer
+
+    const deadline = '2021-02-01';
+
+    let getTimeRemaining = (endtime) => {
+
+        // Рахуємо та повертаємо дні, години, ....
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    };
+
+    let addZero = (num) => {
+        if (num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    };
+
+    let setClock = (selector, endTime) => {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds');
+
+
+        let updateClock = () => {
+            // Викликаємо функцію і отримані дані записуємов в змінну
+            const t = getTimeRemaining(endTime);
+
+            days.innerHTML = addZero(t.days);
+            hours.innerHTML = addZero(t.hours);
+            minutes.innerHTML = addZero(t.minutes);
+            seconds.innerHTML = addZero(t.seconds);
+
+            const timeInterval = setInterval(updateClock, 1000);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        };
+
+        updateClock();
+
+    };
+
+    setClock('.timer', deadline);
+
 });

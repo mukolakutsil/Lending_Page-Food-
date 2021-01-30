81e13e3f39b7e2b98726ec478199eec96fa68d4b
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-01-30 19:58:04 +0200

    slide-navigation

--- js/script.js
+++ js/script.js
@@ -246,6 +246,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Slide
 
     const prev = document.querySelector('.offer__slider-prev'),
+        slider = document.querySelector('.offer__slider'),
         next = document.querySelector('.offer__slider-next'),
         currentNum = document.querySelector('#current'),
         totalNum = document.querySelector('#total'),
@@ -278,7 +279,70 @@ document.addEventListener('DOMContentLoaded', () => {
         slide.style.width = width;
     });
 
+    slider.style.position = 'relative';
 
+    const indicators = document.createElement('ol'),
+        dots = [];
+    indicators.classList.add('carousel-indicators');
+
+    indicators.style.cssText = `
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: 15;
+    display: flex;
+    justify-content: center;
+    margin-right: 15%;
+    margin-left: 15%;
+    list-style: none;
+    `;
+
+    slider.append(indicators);
+
+    for (let i = 0; i < slides.length; i++) {
+        const dot = document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+        box-sizing: content-box;
+        flex: 0 1 auto;
+        width: 30px;
+        height: 6px;
+        margin-right: 3px;
+        margin-left: 3px;
+        cursor: pointer;
+        background-color: #fff;
+        background-clip: padding-box;
+        border-top: 10px solid transparent;
+        border-bottom: 10px solid transparent;
+        opacity: .5;
+        transition: opacity .6s ease;
+        `;
+
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+
+        indicators.append(dot);
+        dots.push(dot);
+    }
+
+    let slideTransform = () => {
+        slidesFiled.style.transform = `translateX(-${offset}px)`;
+    };
+
+    let createDots = () => {
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+    };
+
+    let currentCount = () => {
+        if (slides.length < 10) {
+            currentNum.textContent = `0${slideIndex}`;
+        } else {
+            currentNum.textContent = slideIndex;
+        }
+    };
 
     next.addEventListener('click', () => {
 
@@ -288,9 +352,7 @@ document.addEventListener('DOMContentLoaded', () => {
             offset += +width.slice(0, width.length - 2);
         }
 
-
-
-        slidesFiled.style.transform = `translateX(-${offset}px)`;
+        slideTransform();
 
         if (slideIndex == slides.length) {
             slideIndex = 1;
@@ -298,11 +360,9 @@ document.addEventListener('DOMContentLoaded', () => {
             slideIndex++;
         }
 
-        if (slides.length < 10) {
-            currentNum.textContent = `0${slideIndex}`;
-        } else {
-            currentNum.textContent = slideIndex;
-        }
+        currentCount();
+
+        createDots();
 
     });
 
@@ -314,7 +374,7 @@ document.addEventListener('DOMContentLoaded', () => {
             offset -= +width.slice(0, width.length - 2);
         }
 
-        slidesFiled.style.transform = `translateX(-${offset}px)`;
+        slideTransform();
 
         if (slideIndex == 1) {
             slideIndex = slides.length;
@@ -322,11 +382,28 @@ document.addEventListener('DOMContentLoaded', () => {
             slideIndex--;
         }
 
-        if (slides.length < 10) {
-            currentNum.textContent = `0${slideIndex}`;
-        } else {
-            currentNum.textContent = slideIndex;
-        }
+        currentCount();
+
+        createDots();
+
+    });
+
+
+    dots.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const slideTo = e.target.getAttribute('data-slide-to');
+
+            slideIndex = slideTo;
+            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+            slideTransform();
+
+            currentCount();
+
+            createDots();
+
+        });
+
     });
 
     // const hideSlides = () => {

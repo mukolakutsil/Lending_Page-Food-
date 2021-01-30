fc5a69b38472c479e8aa91dead2fbbce68f13621
Mykola_Kutsil <mukola.kutsil22@gmail.com>
2021-01-30 21:08:39 +0200

    refactor_regular-expressions

--- js/script.js
+++ js/script.js
@@ -344,12 +344,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     };
 
+    let symbolCutting = str => +str.replace(/\D/g, '');
+
     next.addEventListener('click', () => {
 
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == symbolCutting(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += symbolCutting(width);
         }
 
         slideTransform();
@@ -369,9 +371,9 @@ document.addEventListener('DOMContentLoaded', () => {
     prev.addEventListener('click', () => {
 
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = symbolCutting(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= symbolCutting(width);
         }
 
         slideTransform();
@@ -394,7 +396,7 @@ document.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = symbolCutting(width) * (slideTo - 1);
 
             slideTransform();
 
